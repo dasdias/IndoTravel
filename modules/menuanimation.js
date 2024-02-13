62e2cb22de400fd3c964e0acb4d183cd8187983e
Alex Fix <dasdias@yandex.ru>
2024-02-13 15:11:40 +0300

    module 03 lesson 04 animate menu and scroll

--- modules/menuanimation.js
+++ modules/menuanimation.js
@@ -1,22 +1,41 @@
 export const menuAnimation = () => {
   const duration = 400;
   const degrees = 180;
+  const menuScale = 100;
+  let flag = 1;
 
   const headerMenuButton = document.querySelector('.header__menu-button');
+  const headerMenu = document.querySelector('.header__menu');
   headerMenuButton.style.transition = 'unset';
+  headerMenu.style.transition = 'unset';
 
   const startAnimation = (duration, callback) => {
     let startTime = NaN;
+    let progress = NaN;
 
     requestAnimationFrame(function step(timestamp) {
       startTime ||= timestamp;
-      const progress = (timestamp - startTime) / duration;
+      if (flag % 2 === 1) {
+        progress = (timestamp - startTime) / duration;
 
-      if (progress < 1) {
-        requestAnimationFrame(step);
-        callback(progress);
+        if (progress < 1) {
+          flag = 1;
+          requestAnimationFrame(step);
+          callback(progress);
+        } else {
+          flag = 2;
+          callback(Math.floor(progress));
+        }
       } else {
-        callback(Math.floor(progress));
+        progress = (menuScale / 100) - ((timestamp - startTime) / duration);
+        if (progress > 0) {
+          flag = 2;
+          requestAnimationFrame(step);
+          callback(progress);
+        } else {
+          flag = 1;
+          callback(Math.ceil(progress));
+        }
       }
     });
   };
@@ -24,7 +43,9 @@ export const menuAnimation = () => {
   headerMenuButton.addEventListener('click', () => {
     startAnimation(duration, (progress) => {
       const deg = progress * degrees;
+      const scale = progress;
       headerMenuButton.style.transform = `rotate(${deg}deg)`;
+      headerMenu.style.transform = `scale(${scale})`;
     });
   });
 };
