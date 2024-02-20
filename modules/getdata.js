0da50d8f07cb353ad6aea75bcd7d1579fb8b558d
Alex Fix <dasdias@yandex.ru>
2024-02-21 00:03:15 +0300

    module 03 lesson 05

--- modules/getdata.js
+++ modules/getdata.js
@@ -10,26 +10,36 @@ const createDataElements = (data, classes) => {
     const option = document.createElement('option');
     option.classList.add(...classes);
     option.setAttribute('value', item.date);
+    option.setAttribute('data-min-people', item['min-people']);
+    option.setAttribute('data-max-people', item['max-people']);
     option.textContent = item.date;
     return option;
   });
   return elements;
 };
-const createPeopleElements = (data, classes) => {
-  const elements = data.map(item => {
-    // tour__option reservation__option
+const createPeopleElements = (data, classes, countPeople) => {
+  console.log('countPeople: ', countPeople);
+  const elements = [];
+  for (let i = 0; i < countPeople; i++) {
     const option = document.createElement('option');
     option.classList.add(...classes);
-    option.setAttribute('value', item['min-people']);
-    option.textContent = item['min-people'];
-    return option;
-  });
+    option.setAttribute('value', i + 1);
+    option.textContent = i + 1;
+    elements.push(option);
+  }
+  // const elements = data.map(item => {
+  //   // tour__option reservation__option
+  //   return option;
+  // });
   return elements;
 };
 
 export const renderData = async () => {
   const data = await getData();
+  const massMaxPeople = data.map(item => item['max-people']);
+  const maxCountPeople = Math.max.apply(null, massMaxPeople);
   console.log('data: ', data);
+
   const tourDate = document.
     querySelector('.tour__select-wrapper_date .tour__select');
   const tourPeople = document.
@@ -43,7 +53,10 @@ export const renderData = async () => {
   tourDate.innerHTML =
   '<option value="" class="tour__option">Выбери дату</option>';
   tourDate.append(...tourDateElements);
-  const tourPeopleElements = createPeopleElements(data, ['tour__option']);
+
+  const tourPeopleElements = createPeopleElements(data,
+    ['tour__option'], maxCountPeople);
+
   tourPeople.innerHTML = `
     <option value="" class="tour__option">Количество человек</option>`;
   tourPeople.append(...tourPeopleElements);
@@ -53,8 +66,10 @@ export const renderData = async () => {
   reservationDate.innerHTML =
     '<option value="" class="tour__option">Дата путешествия</option>';
   reservationDate.append(...reservationDateElements);
-  const reservationPeopleElements =
-      createPeopleElements(data, ['tour__option', 'reservation__option']);
+
+  const reservationPeopleElements = createPeopleElements(data,
+    ['tour__option', 'reservation__option'], maxCountPeople);
+
   reservationPeople.innerHTML = `<option value=""
     class="tour__option reservation__option">Количество человек</option>`;
   reservationPeople.append(...reservationPeopleElements);
